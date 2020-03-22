50fb1f30497d7e697c71ff8096ffcd359ab776db
Kyle O'Dell <kodell315@gmail.com>
2020-03-22 01:31:18 -0700

    updated door script

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public abstract class Door : MonoBehaviour
+public class Door : MonoBehaviour
 {
-    public Collider2D collider;
-    public Vector3 entrance;
-    public bool isTouching;
-    //public Vector3 exit;
+    public string goingTo;
+    public int goingToIndex;
 
-    public void Open(Collider2D collision, string goingTo, string comingFrom) 
+    public class Coordinates 
+    {
+        public static Vector3 tutorial1 = new Vector3(3.315f, -2.86f, 0f);
+        public static Vector3 firstLevel1 = new Vector3(-3.19f, -3.74f, 0f);
+        public static Vector3 house1 = new Vector3(0f, -1.52f, 0f);
+        public static Vector3 firstLevel2 = new Vector3(43.48f, 4.6f, 0f);
+        public static Vector3 preFinalLevel1 = new Vector3(-6.8f, -2.2f, 0f);
+        public static Vector3 firstLevel3 = new Vector3(0f, 0f, 0f);
+
+        public static List<Vector3> cooridnates = new List<Vector3>(){ tutorial1, firstLevel1, house1, firstLevel2, preFinalLevel1, firstLevel3 };
+    }
+
+    public void Open(Collider2D collision, string goingTo, Vector3 goingToVec) 
     {
         
         if (collision.Equals(Player.Instance.collider))
         {
-
-            if (SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName(comingFrom)))
-            {
-                // Player.Instance.prevLocation = entrance;
-                SceneManager.LoadScene(goingTo);
-                Player.Instance.transform.position = entrance;
-                //Player.Instance.nextLocation = Player.Instance.prevLocation;
-            }
-            else
-            {
-                //Player.Instance.prevLocation = entrance;
-                SceneManager.LoadScene(comingFrom);
-                Player.Instance.transform.position = entrance;
-            }
-
-
+            SceneManager.LoadScene(goingTo);
+            Player.Instance.transform.position = goingToVec;
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Open(collision, goingTo, Coordinates.cooridnates[goingToIndex]);
     }
 }
