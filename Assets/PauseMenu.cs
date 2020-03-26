57d35a2e37977ccada5beb23c3c33c6a2977068a
Kyle O'Dell <kodell315@gmail.com>
2020-03-26 15:20:05 -0700

    added pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenu;
+    public Button resumeButton;
+    public Button menuButton;
+    public Button quitButton;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Resume() 
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause() 
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void MainMenuPressed()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+        Player.Instance.enabled = false;
+        Player.Instance.transform.position = new Vector3(6f, -3.86f, 0f);
+    }
+
+    public void QuitPressed()
+    {
+        Debug.Log("Quit called");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+
+    }
+
+}
