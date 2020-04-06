74d19b7c4024f6eeef6fbc36e751a04de58be3e2
Kyle O'Dell <kodell315@gmail.com>
2020-04-06 01:33:02 -0700

    added health items

---
+++ Assets/Scripts/HealthItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthItem : MonoBehaviour
+{
+    public GameObject item;
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+
+        if (player != null)
+        {
+            player.hp = 150;
+        }
+
+        item.SetActive(false);
+    }
+}
